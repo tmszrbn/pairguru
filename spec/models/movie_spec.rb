d6771ee06b1716668e77d8eaa49a616befa64b26
tmszrbn <urbi90@gmail.com>
2018-08-14 11:25:08 +0200

    fix a to movie

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe Movie do
   it 'validates using TitleBracketsValidator' do
-    a = Movie.create(title: '{{lskdjf')
-    expect(a.errors[:title].include? "Should have matching brackets").to be
+    movie = Movie.create(title: '{{lskdjf')
+    expect(movie.errors[:title].include? "Should have matching brackets").to be
   end
 end
